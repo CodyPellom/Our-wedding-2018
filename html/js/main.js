b4f2c951027e5904d7e167a9fe85f01c489fc302
paulpellom@gmail.com <paulpellom@gmail.com>
2018-02-16 23:01:05 -0500

    weddin wrk

---
+++ html/js/main.js
@@ -0,0 +1,33 @@
+$( document ).ready(function() {
+    
+
+    $('#index-header').hide(); $('#sub-header-index').hide(); 
+    $('#newdelhi').hide(); $('#index-p').hide(); $('#signature').hide();
+
+    $('#sub-header-index').fadeIn(20000); $('#index-header').fadeIn(16000);
+    $('#newdelhi').fadeIn(16000); $('#index-p').fadeIn(12000); $('#signature').fadeIn(45000);
+
+
+    index-header
+    sub-header-index
+    newdelhi
+    index-p
+    signature
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
